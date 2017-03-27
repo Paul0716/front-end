f72c6c92e46cc99e86e4333e3d1633d63f4a493b
paultaku-hp\paultaku <justlove0714@hotmail.com>
2017-03-28 07:32:23 +0800

    1. change webpack config 2. react component

--- react-practice/main.js
+++
@@ -1,9 +0,0 @@
-var React = require("react");
-require("./app.css");
-
-React.render(
-    "<h1>hello world!</h1>",
-    document.body
-);
-
-$("#content").text("jquery ready!");
--- react-practice/webpack.config.js
+++ react-practice/webpack.config.js
@@ -1,45 +1,37 @@
 var webpack = require("webpack");
 var uglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
-
 module.exports = {
-  entry: {
-    bundle: './main.js',
-  },
-  output: {
-    filename: '[name].js'
-  },
-  module: {
-      loaders:[
-        {
-              test: /\.js[x]?$/,
-              exclude: /node_modules/,
-              loader: 'babel-loader'
-        },
-        {
-              test: /\.css$/,             
-              loader: 'style-loader!css-loader',
-        },
-        {
+   entry: {
+       bundle: './main.jsx'
+   },
+   output: {
+      filename: './[name].js',
+   },
+   devServer: {
+      inline: true,
+      port: 8080
+   },
+   module: {
+      loaders: [
+         {
+            test: /\.js[x]?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+               presets: ['es2016', 'react']
+            }
+         },
+         {
+            test: /\.css$/,             
+            loader: 'style-loader!css-loader',
+         },
+         {
             test: /\.(png|jpg)$/,
             loader: 'url-loader?limit=8192'
-        },
-        
+         },
       ]
-  },
-  plugins: [
-      new uglifyJsPlugin(
-          {
-              compress: {
-                  warnings: true,
-              }
-          }
-      ),
-      new webpack.ProvidePlugin(
-          {
-            $: "jquery",
-            jQuery:"jquery",
-            "window.jQuery": "jquery",
-          }
-      )
-  ]
+   },
+   resolve: {
+        extensions: ['.js','.jsx','.json']
+   },
 };
