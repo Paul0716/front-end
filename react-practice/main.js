a3cd0a2feb87e7472c50f68e0f1e571555e71440
paultaku-hp\paultaku <justlove0714@hotmail.com>
2017-03-27 09:11:07 +0800

    1. react-practice

---
+++ react-practice/main.js
@@ -0,0 +1,9 @@
+var React = require("react");
+require("./app.css");
+
+React.render(
+    "<h1>hello world!</h1>",
+    document.body
+);
+
+$("#content").text("jquery ready!");
---
+++ react-practice/webpack.config.js
@@ -0,0 +1,45 @@
+var webpack = require("webpack");
+var uglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+
+module.exports = {
+  entry: {
+    bundle: './main.js',
+  },
+  output: {
+    filename: '[name].js'
+  },
+  module: {
+      loaders:[
+        {
+              test: /\.js[x]?$/,
+              exclude: /node_modules/,
+              loader: 'babel-loader'
+        },
+        {
+              test: /\.css$/,             
+              loader: 'style-loader!css-loader',
+        },
+        {
+            test: /\.(png|jpg)$/,
+            loader: 'url-loader?limit=8192'
+        },
+        
+      ]
+  },
+  plugins: [
+      new uglifyJsPlugin(
+          {
+              compress: {
+                  warnings: true,
+              }
+          }
+      ),
+      new webpack.ProvidePlugin(
+          {
+            $: "jquery",
+            jQuery:"jquery",
+            "window.jQuery": "jquery",
+          }
+      )
+  ]
+};
